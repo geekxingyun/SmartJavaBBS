2893504ed8c9eb5ca025320a0ad88ce6848018ab
qing-feng.zhao <fairy_xingyun@hotmail.com>
2019-12-29 09:57:49 +0800

    init smart-java-bbs project

---
+++ source/web/single-app/smart-java-bbs/src/main/java/com/xingyun/smartjavabbs/SmartJavaBbsApplication.java
@@ -0,0 +1,13 @@
+package com.xingyun.smartjavabbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartJavaBbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartJavaBbsApplication.class, args);
+    }
+
+}
